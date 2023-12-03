1269592d817e31a9eeb4fad5fc4357ba97c940eb
NotPiny <103984634+NotPiny@users.noreply.github.com>
2023-12-03 17:31:11 +0000

    .

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/shoelace.js'
+    let htmlElement = document.getElementsByTagName('html')[0]
+    htmlElement.classList.add('sl-theme-dark')
+</script>
+
+
+
+<style>
+    @import url('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/themes/dark.css');
+</style>
