1fc24938f0dbba58e246f16e96c6eee008a7d1ea
NotPiny <103984634+NotPiny@users.noreply.github.com>
2024-01-09 19:09:17 +0000

    Add error page (oh and more guide pages and stuff but no one will notice)

---
+++ src/lib/data/guideMap.js
@@ -0,0 +1,22 @@
+/**
+ * @type {Array<{name: string, url: string, page: number}>}
+*/
+const guideMap = [
+    {
+        name: "Setting up DaalBot",
+        url: "/guides/setup/intro",
+        page: 1
+    },
+    {
+        name: '1.1 - Adding DaalBot to your server',
+        url: '/guides//adding',
+        page: 1.1
+    },
+    {
+        name: '1.2 - Permissions and Role Hierarchy',
+        url: '/guides/permissions',
+        page: 1.2
+    },
+]
+
+export default guideMap;
